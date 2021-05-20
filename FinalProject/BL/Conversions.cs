fe10f35b753fd680c3b8fd5c85300260bd77ae17
bruria <bruria5460@gmail.com>
2021-05-21 00:07:39 +0300

    first commit

---
+++ FinalProject/BL/Conversions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BL
+{
+    class Conversions
+    {
+        //TODO: Change to generic convert
+        public static UserDTO ConvertUser(User user)
+        {
+            UserDTO userDTO = new DTO.UserDTO()
+            {
+                Uid = user.Uid,
+                Uaddress = user.Uaddress,
+                Uemail = user.Uemail,
+                UfirstName = user.UfirstName,
+                UlastName = user.UlastName,
+                Upassword = user.Upassword,
+                Uphone = user.Uphone,
+                UuserName = user.UuserName
+            };
+            return userDTO;
+        }
+        public static User ConvertUser(UserDTO userDTO)
+        {
+            User user = new User()
+            {
+                Uid = userDTO.Uid,
+                Uaddress = userDTO.Uaddress,
+                Uemail = userDTO.Uemail,
+                UfirstName = userDTO.UfirstName,
+                UlastName = userDTO.UlastName,
+                Upassword = userDTO.Upassword,
+                Uphone = userDTO.Uphone,
+                UuserName = userDTO.UuserName
+            };
+            return user;
+        }
+
+    }
+}
---
+++ FinalProject/BL/UserBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BL
+{
+    public class UserBL
+    {
+        //TODO: change the conversions
+
+        public static IEnumerable<UserDTO> GetAll( )
+        {
+            IEnumerable<User> listUsers = UserDAL.GetAll();
+            foreach (var user in listUsers)
+            {
+                yield return Conversions.ConvertUser(user);
+            }
+        }
+
+        public static UserDTO GetUser(int id)
+        {
+            return Conversions.ConvertUser(UserDAL.GetUser(id));
+        }
+
+        public static void AddUser(UserDTO user)
+        {
+            UserDAL.AddUser(Conversions.ConvertUser(user));
+        }
+
+        public static void UpdateUser( int id, UserDTO user)
+        {
+            UserDAL.UpdateUser(Conversions.ConvertUser(user), id);
+        }
+
+        public static void DeleteUser(int id)
+        {
+            UserDAL.DeleteUser(id);
+        }
+    }
+}
