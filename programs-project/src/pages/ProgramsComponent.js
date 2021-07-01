416139f5bd3cf4385231eff20ec3147f862400f7
bruria <bruria5460@gmail.com>
2021-07-01 23:20:20 +0300

    get-users

---
+++ programs-project/src/pages/ProgramsComponent.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+
+// export const Programs = (props) => {
+
+//     const { programs } = props;
+    
+//     return (
+//         programs.map((program) => (
+//             <div className="programs" key={program.id}>
+//                 <Program
+//                     path={program.path}
+//                     img={program.img}
+                                        
+//                 />
+//             </div>
+//         ))
+//     );
+// }
